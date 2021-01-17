7088c5258319d4fec1141e79a7f92d313d056430
Andy Mardesich <andymardesich@gmail.com>
2021-01-17 11:30:13 -0800

    PureCSS; HTML; fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Coming Soon
-      </header>
+    <div className="container">
+      <div className="pure-g content">
+        <form class="pure-form pure-u-1">
+          <h1>Shorten A URL</h1>
+          <div>
+            <label for="inputBox" class="inputBoxLabel">Need a URL Shortened? Paste it here:</label>
+            <input id="inputBox" type="text" name="longUrl" required="" class="pure-u-3-4 pure-u-sm-1"/>
+            <button type="submit" class="button-xlarge pure-button pure-button-primary pure-u-1-4 pure-u-sm-1">Shorten</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
