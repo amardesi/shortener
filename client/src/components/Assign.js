eddf5db017b993f930e01b186b6d7f3ee48a07be
Andy Mardesich <andymardesich@gmail.com>
2021-01-19 18:09:29 -0800

    Now using useAsyncTaskFetch properly

--- client/src/components/Assign.js
+++ client/src/components/Assign.js
@@ -1,35 +1,26 @@
 import { useAsyncTaskFetch } from "react-hooks-async";
-import { useRef } from "react";
 
 const Found = (props) => (
   <div className="pure-g">
     <div className="pure-u-1">
-      <h2>{props.message} <a href="{props.link}">{props.link}</a></h2>
+      <h2>{props.message} <a href={props.link}>{props.link}</a></h2>
     </div>
   </div>
-);
+)
 
 function Assign() {
-  const host = window.location.host;
-  let shortCode = "";
-  const path = useRef('http://localhost:3000/long');
-  let options = useRef({});
-  const { pending, error, result, start } = useAsyncTaskFetch("");
-  
+  const origin = window.location.origin;
+  const { error, result, start } = useAsyncTaskFetch('http://localhost:3000/long');
+  const shortCode = ((!error && result) ? result.short : '');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    start(path.current, options.current);
-    options.current = {
+    start(null, {
       method: "POST",
       body: JSON.stringify({ longUrl: document.getElementById('inputBox').value}),
-      headers: { "Content-Type": "application/json" }};
-    if (!error && result) {
-      shortCode = result.short;
-    }
-
-  }
-
+      headers: { "Content-Type": "application/json" }
+    });
+  };
   return (
     <form className="pure-form pure-u-1" onSubmit={handleSubmit}>
       <h1>Shorten a URL</h1>
@@ -38,12 +29,10 @@ function Assign() {
         <input id="inputBox" type="text" name="longUrl" required="" className="pure-u-3-4 pure-u-sm-1" />
         <button type="submit" className="button-xlarge pure-button pure-button-primary pure-u-1-4 pure-u-sm-1">Shorten</button>
       </div>
-      {shortCode && shortCode !== "" &&
-        <Found message={"Here's your short URL: "} link={`${host}/${shortCode}`} /> }
-      {pending &&
-        <div>Loading...</div>}
+      {shortCode && shortCode !== "" && 
+        <Found message={"Here's your short URL: "} link={`${origin}/${shortCode}`} /> }
     </form>
   )
-}
+};
 
 export default Assign;
