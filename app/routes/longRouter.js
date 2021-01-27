a0638aea014d9fe2b98a52eec60864b077141b8f
Andy Mardesich <andymardesich@gmail.com>
2021-01-27 15:13:50 -0800

    added error block in longRouter; installed nodemon and eslint

--- app/routes/longRouter.js
+++ app/routes/longRouter.js
@@ -14,38 +14,42 @@ longRouter.route('/')
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
-.post((req, res) => {
+.post(
+  (req, res) => {
   // Look if longUrl is already there
-  axios.get(`${dataUrl}/data`)
-  .then(resp => {
-    // Not as fast as using an indexed DB 🚄
-    return resp.data.find(item => item.longUrl === req.body.longUrl);
-  })
-  .then(target => {
-    if (target) {
-      success(res, target.short, target.longUrl);
-      return Promise.reject();  // Not a graceful exit
-    }
-    return null;
-  })
-  .then(resp => {
-    const uniqueStub = nanoid(7);    // 4.3980465111e+12 possible permutations!
-    axios.post(`${dataUrl}/data`, { short: uniqueStub, longUrl: req.body.longUrl })
-    .then(dataResp => {
-      success(res, `${uniqueStub}`, dataResp.data.longUrl);
+    axios.get(`${dataUrl}/data`)
+    .then(resp => {
+      // Not as fast as using an indexed DB 🚄
+      return resp.data.find(item => item.longUrl === req.body.longUrl);
+    })
+    .then(target => {
+      if (target) {
+        success(res, target.short, target.longUrl);
+        return Promise.reject();  // Not a graceful exit
+      }
+      return null;
+    })
+    .then(resp => {
+      const uniqueStub = nanoid(7);    // 4.3980465111e+12 possible permutations!
+      axios.post(`${dataUrl}/data`, { short: uniqueStub, longUrl: req.body.longUrl })
+      .then(dataResp => {
+        success(res, `${uniqueStub}`, dataResp.data.longUrl);
+      })
+      .catch(err => {
+        console.log(err);
+      });
     })
     .catch(err => {
       console.log(err);
+    })
+  })
+    .put((req, res) => {
+      res.statusCode = 403;
+      res.end('PUT operation not supported on /long');
+    })
+    .delete((req, res) => {
+      res.statusCode = 403;
+      res.end('DELETE operation not supported on /long');
     });
-  }
-)})
-.put((req, res) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on /long');
-})
-.delete((req, res) => {
-  res.statusCode = 403;
-  res.end('DELETE operation not supported on /long');
-});
 
 module.exports = longRouter;
